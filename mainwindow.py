d57cce39cc6fe38feb90dd952428a428e819d9a1
huangjinchao <grozta@163.com>
2024-02-01 17:51:29 +0800

    通过设定setWindowFlags(Qt.CustomizeWindowHint)，让FramelessWindowHint可以实现本地拉伸

--- mainwindow.py
+++ mainwindow.py
@@ -187,7 +187,7 @@ class MainWindow(QWidget, Ui_Form):
         SingelLayout = 3
         """只显示一张图"""
 
-    Margins = 5
+    Margins = 0
     """四周边距"""
     _pressed = False  # 拉升参数
     Direction = None
@@ -441,6 +441,7 @@ class MainWindow(QWidget, Ui_Form):
         # self.setAttribute(Qt.WA_TranslucentBackground)
         # 设置无边框
         self.setWindowFlags(Qt.FramelessWindowHint)
+        self.setWindowFlags(Qt.CustomizeWindowHint)
         # 绑定titel栏中按钮信号
         # 关闭按钮
         self.toolButton_close.clicked.connect(self.exitApp)
@@ -844,116 +845,10 @@ class MainWindow(QWidget, Ui_Form):
         event.accept()
 
     def mouseMoveEvent(self, event):
-        pos = event.pos()
-        xPos, yPos = pos.x(), pos.y()
-        wm, hm = self.width() - self.Margins, self.height() - self.Margins
-        if self.isMaximized() or self.isFullScreen():
-            self.Direction = None
-
-        if xPos <= self.Margins and yPos <= self.Margins:
-            # 左上角
-            self.Direction = self.LeftTop
-            self.setCursor(Qt.SizeFDiagCursor)
-        elif wm <= xPos <= self.width() and hm <= yPos <= self.height():
-            # 右下角
-            self.Direction = self.RightBottom
-            self.setCursor(Qt.SizeFDiagCursor)
-        elif wm <= xPos and yPos <= self.Margins:
-            # 右上角
-            self.Direction = self.RightTop
-            self.setCursor(Qt.SizeBDiagCursor)
-        elif xPos <= self.Margins and hm <= yPos:
-            # 左下角
-            self.Direction = self.LeftBottom
-            self.setCursor(Qt.SizeBDiagCursor)
-        elif 0 <= xPos <= self.Margins and self.Margins <= yPos <= hm:
-            # 左边
-            self.Direction = self.Left
-            self.setCursor(Qt.SizeHorCursor)
-        elif wm <= xPos <= self.width() and self.Margins <= yPos <= hm:
-            # 右边
-            self.Direction = self.Right
-            self.setCursor(Qt.SizeHorCursor)
-        elif self.Margins <= xPos <= wm and 0 <= yPos <= self.Margins:
-            # 上面
-            self.Direction = self.Top
-            self.setCursor(Qt.SizeVerCursor)
-        elif self.Margins <= xPos <= wm and hm <= yPos <= self.height():
-            # 下面
-            self.Direction = self.Bottom
-            self.setCursor(Qt.SizeVerCursor)
-        else:
-            self.Direction = None
-            self.setCursor(self.normalCursor)
+        self.Direction = None
 
         if event.buttons() == Qt.LeftButton and self.headmPos:
             self.move(self.mapToGlobal(event.pos() - self.headmPos))
-
-        if event.buttons() == Qt.LeftButton and self._pressed and self.Direction:
-            self._resizeWidget(event.pos())
-        # event.accpet()
-
-    def _resizeWidget(self, pos):
-        """调整窗口大小"""
-        if self.Direction == None:
-            return
-        mpos = pos - self._mpos
-        xPos, yPos = mpos.x(), mpos.y()
-        geometry = self.geometry()
-        x, y, w, h = geometry.x(), geometry.y(), geometry.width(), geometry.height()
-        if self.Direction == self.LeftTop:  # 左上角
-            if w - xPos > self.minimumWidth():
-                x += xPos
-                w -= xPos
-            if h - yPos > self.minimumHeight():
-                y += yPos
-                h -= yPos
-        elif self.Direction == self.RightBottom:  # 右下角
-            if w + xPos > self.minimumWidth():
-                w += xPos
-                self._mpos = pos
-            if h + yPos > self.minimumHeight():
-                h += yPos
-                self._mpos = pos
-        elif self.Direction == self.RightTop:  # 右上角
-            if h - yPos > self.minimumHeight():
-                y += yPos
-                h -= yPos
-            if w + xPos > self.minimumWidth():
-                w += xPos
-                self._mpos.setX(pos.x())
-        elif self.Direction == self.LeftBottom:  # 左下角
-            if w - xPos > self.minimumWidth():
-                x += xPos
-                w -= xPos
-            if h + yPos > self.minimumHeight():
-                h += yPos
-                self._mpos.setY(pos.y())
-        elif self.Direction == self.Left:  # 左边
-            if w - xPos > self.minimumWidth():
-                x += xPos
-                w -= xPos
-            else:
-                return
-        elif self.Direction == self.Right:  # 右边
-            if w + xPos > self.minimumWidth():
-                w += xPos
-                self._mpos = pos
-            else:
-                return
-        elif self.Direction == self.Top:  # 上面
-            if h - yPos > self.minimumHeight():
-                y += yPos
-                h -= yPos
-            else:
-                return
-        elif self.Direction == self.Bottom:  # 下面
-            if h + yPos > self.minimumHeight():
-                h += yPos
-                self._mpos = pos
-            else:
-                return
-        self.setGeometry(x, y, w, h)
 
     def add_main_file(self):
         """
